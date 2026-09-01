85c306d66fbc06a21c4ae449995225bef5dda6b0
DManeshev <maneshev2000@mail.ru>
2026-09-01 12:38:00 +0300

    git

--- src/app/dashboard/productaction/ProductAction.tsx
+++ src/app/dashboard/productaction/ProductAction.tsx
@@ -263,7 +263,6 @@ export default function ProductAction() {
 								</SelectContent>
 							</Select>
 							<FieldError>{fieldState.error?.message}</FieldError>
-							
 						</div>
 					)}
 				/>
