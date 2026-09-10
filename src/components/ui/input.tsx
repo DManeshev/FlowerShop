9b1449a7240578dcc91d908224b758bcd8140cd3
DManeshev <maneshev2000@mail.ru>
2026-09-10 09:42:42 +0300

    fix build

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
+    /* @ts-ignore */
     <InputPrimitive
       type={type}
       data-slot="input"
